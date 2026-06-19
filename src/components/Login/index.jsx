9064682736430c92ace0e7f2dbdb780f9522c409
KESHAVAKASH10 <keshavakash710@gmail.com>
2026-06-19 14:05:09 +0530

    Login Page (Initially)

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,15 @@
+export default function Login() {
+    return (
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '100vh',
+            color: '#00e5a0',
+            fontFamily: 'Sora, sans-serif',
+            fontSize: '1.2rem'
+        }}>
+            Login coming soon...
+        </div>
+    )
+}
